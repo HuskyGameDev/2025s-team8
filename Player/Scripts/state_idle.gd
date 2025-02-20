7947dab3934573c9f30cf031be7687ac7df5cde0
blor01 <blor@mtu.edu>
2025-02-19 19:04:03 -0500

    Converting movement into a state machine
    known bugs:
    -functions not properly referencing another script

---
+++ Player/Scripts/state_idle.gd
@@ -0,0 +1,23 @@
+class_name state_idle extends State
+
+@onready var walk: State = $"../Walk"
+
+func Enter() -> void:
+	player.UpdateAnimation("Idle")
+	pass
+	
+func Exit() -> void:
+	pass
+	
+func Process(_delta : float) -> State:
+	if player.direction != Vector2.ZERO:
+		return walk
+	player.velocity = Vector2.ZERO
+	return null
+	
+func Physics(_delta: float) -> State:
+	return null
+	
+func HandleInput(_event: InputEvent) -> State:
+	return null
+	
