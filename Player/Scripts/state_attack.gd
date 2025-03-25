3bc91e97e810690b8a82168faaff17aaf8c7a064
blor01 <blor@mtu.edu>
2025-03-25 18:59:02 -0400

    basic attack
    attack animation no effects, no audio
    -goal is to test out hit and hurt box
    -need to implement an enemy first

---
+++ Player/Scripts/state_attack.gd
@@ -0,0 +1,44 @@
+class_name state_attack extends State
+
+#tutorial is using animatino player
+#consider switching to time
+
+var attacking: bool = false
+@onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
+
+@onready var idle: State = $"../Idle"
+
+@onready var walk: State = $"../Walk"
+
+func Enter() -> void:
+	player.UpdateAnimation("attack")
+	animation_player.animation_finished.connect(EndAttack)
+	attacking = true
+	pass
+	
+func Exit() -> void:
+	animation_player.animation_finished.disconnect( EndAttack)
+	attacking = false
+	pass
+	
+func Process(_delta : float) -> State:
+	player.velocity = Vector2.ZERO
+	
+	
+	if attacking == false:
+		if player.direction == Vector2.ZERO:
+			return idle
+		else:
+			return walk
+	
+	return null
+	
+func Physics(_delta: float) -> State:
+	return null
+	
+func HandleInput(_event: InputEvent) -> State:
+	return null
+	
+	#
+func EndAttack(_newAnimName : String) -> void:
+	attacking = false
--- Player/Scripts/state_walk.gd
+++ Player/Scripts/state_walk.gd
@@ -2,6 +2,7 @@ class_name state_walk extends State
 
 @export var move_speed : float = 100
 @onready var idle : State = $"../Idle"
+@onready var attack: State = $"../attack"
 
 func Enter() -> void:
 	player.UpdateAnimation("walk")
@@ -20,10 +21,12 @@ func Process(_delta : float) -> State:
 		player.UpdateAnimation("walk")
 
 	return null
-	
 func Physics(_delta: float) -> State:
 	return null
 	
 func HandleInput(_event: InputEvent) -> State:
+	#repeat code from idle for attack
+	if _event.is_action_pressed("attack"):
+			return attack
 	return null
 	
