05181a97f7cf8f53dff36f5e1a54178819e8db54
DerekH81 <dhongist@mtu.edu>
2025-09-15 23:50:55 -0400

    Added flame enemy
    -added a flame enemy that shoots eight fireballs
    -changed a few enemy things

--- Enemies/Flame/flame_enemy.gd
+++ Enemies/Flame/flame_enemy.gd
@@ -6,7 +6,7 @@ var wander_dir_timer : float = 0
 var randAngle = randf() * TAU
 
 @export var projectile_scene: PackedScene
-@export var shoot_delay: float = 1
+var shoot_delay: float = 1.5
 var shoot_timer: float = shoot_delay
 
 
@@ -47,14 +47,15 @@ func try_shoot(delta: float):
 		
 		
 func shoot():
-	var projectile = projectile_scene.instantiate() as Projectile
-	if(projectile != null):
-		get_parent().add_child(projectile)
-	
-	var dir = (player.position - position).normalized()
-	projectile.global_position = global_position + dir
-	projectile.global_rotation = global_rotation
-	projectile.direction = dir
+	for i in range(8):
+		var projectile = projectile_scene.instantiate() as Projectile
+		if(projectile != null):
+			get_parent().add_child(projectile)
+		
+		var dir = Vector2(0,1).rotated(deg_to_rad(45*i))
+		projectile.global_position = global_position + dir
+		projectile.global_rotation = global_rotation
+		projectile.direction = dir
 	
 	
 	
@@ -62,9 +63,3 @@ func _on_detection_area_body_entered(_body: Node2D) -> void:
 	if(player == null and _body is Player):
 		player = _body
 		player_found = true
-		
-		
-#func _on_detection_area_body_exited(_body: Node2D) -> void:
-	#if(_body is Player):
-		#player = null
-		#chase_player = false	
--- GeneralNodes/enemy.gd
+++ GeneralNodes/enemy.gd
@@ -5,8 +5,7 @@ class_name Enemy extends CharacterBody2D
 @export var player: Node2D = null
 #@onready var mc: Player = $"."
 
-# Called when the node enters the scene tree for the first time.
-var hp: int = 3
+@export var hp: int = 3
 
 @export var enemy_name : String
 
@@ -25,7 +24,7 @@ func TakeDamage(_damage:int) -> void:
 		
 		
 func flash_red():
-	$Sprite2D.modulate = Color(1,0,0)
+	$Sprite2D.modulate = Color(200,0,0) # reddw
 	await get_tree().create_timer(0.1).timeout
-	$Sprite2D.modulate = Color(1,1,1,1)
+	$Sprite2D.modulate = Color(1,1,1,1) # back to normal
 		
