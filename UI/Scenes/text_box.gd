fad984b6ac59aac25286f375e7f603edce927801
Mack Abercrombie <Mdabercr@mtu.edu>
2025-09-17 17:36:39 -0400

    Animated text
    Was finally able to figure out tweens and add a typewriter effect with adjustable speed. Makes it look nicer.

--- UI/Scenes/text_box.gd
+++ UI/Scenes/text_box.gd
@@ -1,41 +1,74 @@
 extends Control
 
-#The array of text for each scene
 @export var texts: Array[String] = []
-var cur_ind = 0
-var waiting = false
+@export var speed := 0.1  # seconds per character
+
+var cur_ind = 0			# The current string in the array
+var waiting = false		# If waiting for user input
+var typing = false		# If tween animation is running
+var full_text = ""		# The string currently being displayed
+var char_index = 0		# Index within the displaying string
 
 @onready var label = $Panel/RichTextLabel
+@onready var tween = create_tween()  # Create a tween dynamically
+# Tween just actually gradually increases char_index so process knows how much to show
 
-
-# Called when the node enters the scene tree for the first time.
+# Hide is there only for if there is no text
 func _ready() -> void:
 	hide()
 	start_text(texts)
 
-
-func start_text(newText: Array[String]):
-	texts = newText
+func start_text(new_text: Array[String]):
+	# Need to double check but may just be able to use new_text rather than a new var
+	texts = new_text
 	cur_ind = 0
 	if texts.size() > 0:
 		show()
-		show_text(newText[0])
+		show_text(texts[0])
 	else:
 		hide()
 
 func show_text(text: String):
-	label.text = text
+	# Stop any active tween
+	if tween:
+		tween.kill()
+
+	full_text = text
+	char_index = 0
+	typing = true
+	waiting = false
+	label.text = ""
+
+	# Tween char_index from 0 to full_text.length
+	tween = create_tween()
+	tween.tween_property(self, "char_index", full_text.length(), full_text.length() * speed)
+	tween.set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	tween.connect("finished", Callable(self, "_on_tween_finished"))
+
+func _on_tween_finished():
+	typing = false
 	waiting = true
+	label.text = full_text  # Ensure full text shown at the end
+
+func _process(delta):
+	if typing:
+		# Update label text to current char_index
+		var chars_to_show = int(char_index)
+		label.text = full_text.substr(0, chars_to_show)
 
 func _input(event: InputEvent):
-	if waiting and event.is_action_released("ui_accept"):
-		cur_ind += 1
-		if cur_ind < texts.size():
-			show_text(texts[cur_ind])
-		else:
-			hide()
-			waiting = false
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# Event can be changed from ui_accept (Enter) if needed
+	if event.is_action_released("ui_accept"):
+		if typing:
+			# Skip typing effect
+			tween.kill()
+			label.text = full_text
+			typing = false
+			waiting = true
+		elif waiting:
+			cur_ind += 1
+			if cur_ind < texts.size():
+				show_text(texts[cur_ind])
+			else:
+				hide()
+				waiting = false
